90639bce3e2df435c2521a2ff12125deeece7a06
Asad Imran <iasad4855@gmail.com>
2025-10-18 20:01:27 +0500

    Fixed CORS preflight handling

--- Server/middlewares/multer.js
+++ Server/middlewares/multer.js
@@ -1,12 +1,18 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const uploadfolder = "uploads";
-if (!fs.existsSync(uploadfolder)) fs.mkdirSync(uploadfolder);
+
+// Use the only writable directory in Vercel
+const uploadDir = "/tmp/uploads";
+
+// Vercel may not create the /tmp folder structure, so we do it ourselves
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadfolder);
+    cb(null, uploadDir); // Save to the temporary directory
   },
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${Math.round(
